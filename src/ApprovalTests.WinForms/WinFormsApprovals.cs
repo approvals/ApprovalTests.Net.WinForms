6c402259a83e8d71a0e34cabe0e0a1c935defdf6
Llewellyn Falco <llewellyn.falco@gmail.com>
2019-06-07 00:34:59 -0700

    !!! updated approvaltests

--- src/ApprovalTests.WinForms/WinFormsApprovals.cs
+++ src/ApprovalTests.WinForms/WinFormsApprovals.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms;
 using ApprovalTests.Events;
 using ApprovalTests.Namers;
 using ApprovalUtilities.Reflection;
+using ApprovalUtilities.Utilities;
 
 namespace ApprovalTests.WinForms
 {
